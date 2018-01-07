a579d670839dd3237c7f7b6ad53aea8e0e2d775f
Ryan Wright <rywright@gmail.com>
2018-01-07 12:30:07 -0800

    Initial commit. Experimenting zip parsing in Go

---
+++ parse.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+
+func ExampleReader() {
+	// Open a zip archive for reading.
+	r, err := zip.OpenReader("./irccloud.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+
+	// Iterate through the files in the archive,
+	// printing some of their contents.
+	for _, f := range r.File {
+		fmt.Printf("Contents of %s:\n", f.Name)
+		rc, err := f.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = io.CopyN(os.Stdout, rc, 68)
+		if err != nil {
+			print("\nerror!\n")
+			log.Fatal(err)
+		}
+		rc.Close()
+		fmt.Println()
+	}
+}
+
+
+
+func main() {
+	ExampleReader()
+}
